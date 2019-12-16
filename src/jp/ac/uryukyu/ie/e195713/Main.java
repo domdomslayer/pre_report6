0edde235d33795068c8a5f7af0b5b642d30803b8
Hoshi Iju <e195713@ie.u-ryukyu.ac.jp>
2019-12-17 08:39:36 +0900

    made classes and their constructor

---
+++ src/jp/ac/uryukyu/ie/e195713/Main.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e195713;
+
+public class Main {
+    public static void main(String[] args) {
+	var enemy = new Enemy();
+	var player = new Player();
+	System.out.println(enemy.getNumber());
+    }
+}
